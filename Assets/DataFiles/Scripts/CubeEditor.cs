d1960ac32a18814f9efed9e52c8ea14fb50e2c56
rafaelferreiravitor <ferreiravrafael@gmail.com>
2020-05-20 08:03:56 -0300

    Added enemy and a simple path to it

--- Assets/DataFiles/Scripts/CubeEditor.cs
+++ Assets/DataFiles/Scripts/CubeEditor.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
     [SerializeField] [Range(1f, 20f)] float GridSize;
 
-    private void Awake()
-    {
-        Debug.Log("Awake");
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("Awake");
-    }
+    TextMesh textMesh;
 
-    // Update is called once per frame
+    private void Start()
+    {
+
+        
+    }
     void Update()
     {
-        Debug.Log(".");
         Vector3 snap;
         snap.x = Mathf.RoundToInt(transform.position.x / GridSize)* GridSize;
         snap.y = 0;
         snap.z = Mathf.RoundToInt(transform.position.z / GridSize) * GridSize;
         transform.position = snap;
+        textMesh = GetComponentInChildren<TextMesh>();
+        string labelText = snap.x / GridSize + "," + snap.z / GridSize;
+        textMesh.text = labelText;
+        gameObject.name = "Cube ("+labelText+")";
     }
 }
---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Block> Path;
+
+    int i = 0;
+
+    private void Start()
+    {
+        InvokeRepeating("PlayPath",1,1);
+        
+    }
+
+    public void PlayPath()
+    {
+        gameObject.transform.position = Path[i].GetComponent<Transform>().position;
+        Debug.Log(Path[i].GetComponent<Transform>().position.ToString());
+        if (i < Path.Count-1)
+            i++;
+    }
+}
